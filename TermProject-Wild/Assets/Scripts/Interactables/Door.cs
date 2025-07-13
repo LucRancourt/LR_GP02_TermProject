9fa23fcd957149f5b41b2bc6764d4ce626d50f7a
LucRancourt <welpidkwhat@gmail.com>
2025-07-13 16:08:42 -0400

    Created Door Interactable

--- TermProject-Wild/Assets/Scripts/Interactables/Door.cs
+++ TermProject-Wild/Assets/Scripts/Interactables/Door.cs
@@ -1,16 +1,70 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class Door : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Variables
+    private bool _isOpen = false;
+    [SerializeField] private bool canBeReOpened = false;
+    private bool _hasBeenOpened = false;
+
+    private Sequence _openingSequence;
+    private Sequence _closingSequence;
+    [SerializeField] private float doorUpwardAmount = 10.0f;
+    [SerializeField] private float doorMovementDuration = 4.0f;
+    [SerializeField] private float shakeStrength = 1.0f;
+    [SerializeField] private float shakeDuration = 2.0f;
+    private Vector3 shakeStrengthVector = Vector3.zero;
+
+
+    // Functions
+    private void Start()
     {
-        
+        shakeStrengthVector.x = shakeStrength;
+        shakeStrengthVector.z = shakeStrength;
+
+
+        _openingSequence = DOTween.Sequence();
+        _closingSequence = DOTween.Sequence();
+
+        DOTween.defaultAutoPlay = AutoPlay.None;
+
+
+        // Shake
+        _openingSequence.Append(transform.DOShakePosition(shakeDuration, shakeStrengthVector));
+        _openingSequence.AppendInterval(shakeDuration - 0.25f);
+
+        _closingSequence.Append(transform.DOShakePosition(shakeDuration, shakeStrengthVector));
+        _closingSequence.AppendInterval(shakeDuration - 0.25f);
+
+
+        // Open
+        _openingSequence.Append(transform.DOMoveY(transform.position.y + doorUpwardAmount, doorMovementDuration));
+
+        // Close
+        _closingSequence.Append(transform.DOMoveY(transform.position.y, doorMovementDuration));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleDoor()
     {
-        
+        if (_closingSequence.IsPlaying() || _openingSequence.IsPlaying()) return;
+
+        // Close Door
+        if (_isOpen)
+        {
+            _isOpen = false;
+
+            _closingSequence.Restart();
+        }
+        // Open Door
+        else
+        {
+            if (_hasBeenOpened && !canBeReOpened) return;
+
+            _isOpen = true;
+            _hasBeenOpened = true;
+
+            _openingSequence.Restart();
+        }
     }
 }
